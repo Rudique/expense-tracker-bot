bb26b99347ab91d6016cae80568a700d6c1caa8a
Evgenii Rudenko <mr.senior.rudik@icloud.com>
2026-04-15 23:16:28 +0400

    feat: add user update on /start

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -33,4 +33,4 @@ async def cmd_start(
     if created:
         await message.answer(f"привет, {user.first_name or 'user'}! ты добавлен в базу")
     else:
-        await message.answer(f"с возвращением, {user.first_name or 'user'}!")
+        await message.answer(f"с возвращением, {user.first_name or 'user'}!!!")
